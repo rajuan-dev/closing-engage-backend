bf48d4619aea2459410f43c3472c873e3bdc5d1f
Md Rajuan Hossen <rajuan.official@gmail.com>
2026-07-28 17:47:13 +0600

    feat: initialize express application with security, logging, and swagger documentation middleware

--- src/app.ts
+++ src/app.ts
@@ -20,8 +20,8 @@ app.use(
   }),
 );
 app.use(compression());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(requestLoggerMiddleware);
 
 app.get('/docs.json', (_req, res) => {
